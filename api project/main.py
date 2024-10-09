499ddb08ae9754714994d6545d1707d6d5f0ecbf
amin <aminzakbln@gmail.com>
2024-10-09 15:04:17 +0330

    fixed swagger problem

--- api project/main.py
+++ api project/main.py
@@ -1,31 +1,33 @@
 from flask import Flask, request, jsonify  
-from flask_swagger import swagger  
-from flasgger import Swagger  
 import sqlite3  
 from sqlite3 import Error  
-
-app = Flask(__name__)  
-swagger = Swagger(app)  
+from flasgger import Swagger  
 
 path = "products.db"  
+app = Flask(__name__)  
+swagger = Swagger(app)  
 
 shop = []  
 categorie = [{}]  
 
-# Database setup  
 try:  
     con = sqlite3.connect(path)  
+
     cur = con.cursor()  
+    
     cur.execute("""CREATE TABLE IF NOT EXISTS categorie(  
-                    id INTEGER PRIMARY KEY AUTOINCREMENT, categorie TEXT)""")   
+                id INTEGER PRIMARY KEY AUTOINCREMENT, categorie TEXT)""")   
+    
     cur.execute("""CREATE TABLE IF NOT EXISTS all_products  
-                    (product TEXT, price INTEGER, categorie_id INTEGER,   
-                    FOREIGN KEY(categorie_id) REFERENCES categorie(id)) """)  
+                (product TEXT, price INTEGER, categorie_id INTEGER,   
+                FOREIGN KEY(categorie_id) REFERENCES categorie(id)) """)  
+
     con.commit()   
 except Error:  
     print(Error)   
 finally:  
     con.close()   
+
 
 @app.route('/category/get_all', methods=['GET'])  
 def get_product():  
@@ -35,16 +37,28 @@ def get_product():
     responses:  
       200:  
         description: A list of categories  
+        schema:  
+          type: array  
+          items:  
+            type: object  
+            properties:  
+              id:  
+                type: integer  
+              categorie:  
+                type: string  
     """  
     try:  
         con = sqlite3.connect(path)  
+
         cur = con.cursor()  
         cur.execute("""SELECT id,categorie FROM categorie """)  
         categories = cur.fetchall()  
         categories_list = [{"id": category[0], "categorie": category[1]} for category in categories]   
-        return jsonify(categories_list), 200  
+
+        return jsonify(categories_list)  
     except Error:  
-        return jsonify({"message": "Error occurred during fetching categories"}), 500  
+        print(Error)  
+
 
 @app.route('/category/create', methods=["POST"])  
 def create_categorie():  
@@ -53,62 +67,70 @@ def create_categorie():
     ---  
     parameters:  
       - name: categorie  
-        type: string  
+        in: body  
         required: true  
-        description: The name of the category  
+        schema:  
+          type: object  
+          properties:  
+            categorie:  
+              type: string  
     responses:  
       201:  
         description: The created category  
-      400:  
-        description: Invalid input  
+        schema:  
+          type: object  
+          properties:  
+            id:  
+              type: integer  
+            categorie:  
+              type: string  
     """  
-    if not request.json or 'categorie' not in request.json:  
-        return jsonify({"message": "invalid input"}), 400  
-
     new_categorie = {  
         "categorie": request.json['categorie']  
     }  
+    categorie.append(new_categorie)  
     try:  
         con = sqlite3.connect(path)  
+
         cur = con.cursor()  
         cur.execute("""INSERT INTO categorie(categorie)  
                     VALUES(?)""", (new_categorie['categorie'],))  
         con.commit()  
     except Error:  
-        return jsonify({"message": "Error occurred during category creation"}), 500  
+        print(Error)  
     finally:  
         con.close()  
         return jsonify(new_categorie), 201  
 
-@app.route('/new/product', methods=['POST'])  
+
+@app.route('/create/new/product', methods=['POST'])  
 def create_product():  
     """  
     Create a new product  
     ---  
     parameters:  
       - name: product  
-        type: string  
+        in: body  
         required: true  
-      - name: price  
-        type: integer  
-        required: true  
-      - name: categorie  
-        type: string  
-        required: true  
+        schema:  
+          type: object  
+          properties:  
+            product:  
+              type: string  
+            categorie:  
+              type: string  
+            price:  
+              type: integer  
     responses:  
       201:  
         description: Product created successfully  
-      404:  
-        description: Category not found  
     """  
-    if not request.json or 'product' not in request.json or 'price' not in request.json or 'categorie' not in request.json:  
-        return jsonify({"message": "Invalid input"}), 400  
-
     new_product = {  
         "product": request.json['product'],  
         "categorie": request.json["categorie"],  
         "price": request.json['price']  
     }  
+    shop.append(new_product)  
     con = sqlite3.connect(path)  
 
     cur = con.cursor()  
@@ -116,7 +138,7 @@ def create_product():
     category = cur.fetchone()  
 
     if category is None:  
-        return jsonify({"message": "not found any"}), 404  
+        return jsonify({"message": "Category not found"}), 404  
 
     category_id = category[0]   
     cur.execute("""INSERT INTO all_products(product, price, categorie_id)  
@@ -124,8 +146,8 @@ def create_product():
     
     con.commit()  
     con.close()  
-    return jsonify({"message": "create successfully"}), 201  
-    
+    return jsonify({"message": "Product created successfully"}), 201  
+
 
 @app.route('/get/product/<string:category>', methods=['GET'])  
 def get_product_by_categorie(category):  
@@ -134,20 +156,34 @@ def get_product_by_categorie(category):
     ---  
     parameters:  
       - name: category  
+        in: path  
         type: string  
         required: true  
     responses:  
       200:  
-        description: A list of products  
+        description: A list of products in the specified category  
+        schema:  
+          type: array  
+          items:  
+            type: object  
+            properties:  
+              product:  
+                type: string  
+              price:  
+                type: integer  
+      404:  
+        description: Category not found  
     """  
     con = sqlite3.connect(path)  
+
     cur = con.cursor()  
     cur.execute("""SELECT product,price FROM all_products  
-                WHERE categorie_id = (SELECT id FROM categorie WHERE categorie = ?) """, (category,))  
+                WHERE categorie_id = (SELECT id FROM categorie WHERE categorie = ?) """,(category,))  
     products = cur.fetchall()  
     product_list = [{"product": product[0], "price": product[1]} for product in products]   
     con.close()  
-    return jsonify(product_list), 200  
+    return jsonify(product_list)  
+
 
 @app.route('/update/product', methods=['PUT'])  
 def update_product():  
@@ -156,23 +192,23 @@ def update_product():
     ---  
     parameters:  
       - name: product  
-        type: string  
+        in: body  
         required: true  
-      - name: categorie  
-        type: string  
-        required: false  
-      - name: price  
-        type: integer  
-        required: false  
+        schema:  
+          type: object  
+          properties:  
+            product:  
+              type: string  
+            categorie:  
+              type: string  
+            price:  
+              type: integer  
     responses:  
       200:  
         description: Product updated successfully  
       404:  
         description: Product not found  
     """  
-    if not request.json or 'product' not in request.json:  
-        return jsonify({"message": "Invalid input"}), 400  
-        
     product_name = request.json['product']  
     product_to_update = next(  
         (product for product in shop if product['product'] == product_name), None)  
@@ -182,9 +218,10 @@ def update_product():
             'categorie', product_to_update['categorie'])  
         product_to_update['price'] = request.json.get(  
             'price', product_to_update['price'])  
-        return jsonify({"message": "product updated ", "product": product_to_update}), 200  
+        return jsonify({"message": "Product updated", "product": product_to_update}), 200  
     else:  
-        return jsonify({"message": "product not found"}), 404  
+        return jsonify({"message": "Product not found"}), 404  
+
 
 if __name__ == "__main__":  
-    app.run(debug=True)  
+    app.run()
